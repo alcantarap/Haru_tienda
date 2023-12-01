fde926a99098b24d35949db5210a5120f539c721
diegosly <die.conchar@duocuc.cl>
2023-12-01 00:09:06 -0300

    ..

--- Views/principal/publicaciones_adop.php
+++ Views/principal/publicaciones_adop.php
@@ -1,4 +1,4 @@
-<?php include_once 'Views/template-principal/header.php'; ?>
+<?php include_once 'Views/template/header-principal.php'; ?>
 
 
 <!-- Container -->
@@ -76,4 +76,4 @@
         </div>
 </div>  
 
-<?php include_once 'Views/template-principal/footer.php'; ?>
+<?php include_once 'Views/template/footer-principal.php'; ?>
